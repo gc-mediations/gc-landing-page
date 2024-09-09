d0ac3b10f8b2106208627e2970891431e3809dc0
bottolo <samuele.castiglia.98@gmail.com>
2024-09-09 17:00:34 +0200

    refactor: Assets

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	assetsInclude: ["src/assets/images/**/*"],
 	plugins: [
 		react(),
 		TanStackRouterVite({
@@ -15,11 +16,6 @@ export default defineConfig({
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
-		},
-	},
-	build: {
-		rollupOptions: {
-			external: [/^src\/assets\/images\/.+/],
 		},
 	},
 });
