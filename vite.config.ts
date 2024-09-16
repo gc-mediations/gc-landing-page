71765dc6e5605cde3b29ed3894b6ddf7c5a67ccd
bottolo <samuele.castiglia.98@gmail.com>
2024-09-16 17:19:48 +0200

    fix: Infinite redirects

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
 	assetsInclude: ["src/assets/images/**/*"],
-	// base: "/",
+	base: "/",
 	plugins: [
 		react(),
 		TanStackRouterVite({
